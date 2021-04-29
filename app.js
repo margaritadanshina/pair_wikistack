0073c611486253952f8de6fa77be34af12c116bf
Hannah G Kim <hannahk0108@gmail.com>
2021-04-29 16:06:58 -0500

    Hannahs commit

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const app = express();
+//const { db } = require("./models");
+const { db, Page, User } = require("./models");
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./public")));
@@ -11,13 +17,25 @@ app.get("/", function (req, res) {
   res.send(" ");
 });
 
+// sync allows interaction with data,
+const init = async () => {
+  await Page.sync();
+  await User.sync();
+  //await sequelize.sync({ force: true });  // <- ???
+  await db.sync({ force: true });
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+
+init();
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
-
+// app.listen(PORT, () => {
+//   console.log(`App listening in port ${PORT}`);
+// });
 
 // app.use(express.static(__dirname + "/public"));
 // app.use(express.urlencoded({ extended: false }));
